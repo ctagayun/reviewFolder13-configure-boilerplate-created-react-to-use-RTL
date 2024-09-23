82e755abcfbdf5eea3cf2a9320dafab4470d4775
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-23 10:58:13 -0500

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
@@ -19,7 +20,9 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()],
   devServer: {
     static: path.resolve(__dirname, './dist'),
     hot: true,
