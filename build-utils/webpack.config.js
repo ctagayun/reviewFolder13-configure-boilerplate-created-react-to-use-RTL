7eb590442e3aeafc51e773abb1ed295f32867162
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-24 15:17:25 -0500

    Completed Module 6 - Webpack Addons

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -2,11 +2,28 @@ const { merge } = require('webpack-merge');
 
 const commonConfig = require('./webpack.common.js');
 
-module.exports = ({ env }) => {
+const getAddons = (addonsArgs) => {
+  const addons = Array.isArray(addonsArgs)
+    ? addonsArgs
+    : [addonsArgs];
+
+  return addons
+    .filter(Boolean)
+    .map((name) => require(`./addons/webpack.${name}.js`));
+};
+
+//module.exports = ({ env }) => {
+ module.exports = ({ env, addon }) => {
   const envConfig = require(`./webpack.${env}.js`);
 
-  return merge(commonConfig, envConfig);
+  //return merge(commonConfig, envConfig);
+  return merge(commonConfig, envConfig, ...getAddons(addon));
 };
+
+//Add ons stuff
+//Now, not only the common and environment specific Webpack configuration get merged, 
+//but also the optional addons which we will put into a dedicated 
+//build-utils/addons folder.
 
 //You can see that the function receives our env environment flag from the npm script. 
 //That way, we can dynamically require a environment specific Webpack configuration 
