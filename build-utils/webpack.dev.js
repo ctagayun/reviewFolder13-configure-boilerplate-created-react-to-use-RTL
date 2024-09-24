b7d784c727a18f79b0e36e77de3fe95d821ecd16
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-24 14:53:42 -0500

    Completed WP Env Variables: Env

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,15 +1,29 @@
-const { DefinePlugin } = require('webpack'); //added
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
-    mode: 'development',
-    plugins: [
-      new DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('development'),
-        }
-      }),
-    ],
-    devtool: 'eval-source-map',
-  };
+  mode: 'development',
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, '..', './.env.development'),
+    })
+  ],
+  devtool: 'eval-source-map',
+  performance: { hints: false }
+};
+
+// const { DefinePlugin } = require('webpack'); //added
+// module.exports = {
+//     mode: 'development',
+//     plugins: [
+//       new DefinePlugin({
+//         'process.env': {
+//           'NODE_ENV': JSON.stringify('development'),
+//         }
+//       }),
+//     ],
+//     devtool: 'eval-source-map',
+//   };
 
   //Sometimes you may want to know in your source code whether you are 
   // in development or production mode. For these cases you can specify dynamic 
@@ -24,3 +38,4 @@ module.exports = {
         //     }
         //   }),
         // ],
+
--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,15 +1,30 @@
-const { DefinePlugin } = require('webpack'); //added 
+
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
-    mode: 'production',
-    plugins: [
-      new DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production'),
-        }
-      }),
-    ],
-    devtool: 'source-map',
-  };
+  mode: 'production',
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, '..', './.env.production'),
+    })
+  ],
+  devtool: 'eval-source-map',
+  performance: { hints: false }
+};
+
+// const { DefinePlugin } = require('webpack'); //added 
+// module.exports = {
+//     mode: 'production',
+//     plugins: [
+//       new DefinePlugin({
+//         'process.env': {
+//           'NODE_ENV': JSON.stringify('production'),
+//         }
+//       }),
+//     ],
+//     devtool: 'source-map',
+//   };
 
 
   //give it the above content. 
